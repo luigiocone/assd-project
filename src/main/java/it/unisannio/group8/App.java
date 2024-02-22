91d568d1a01c57f14f64ff41bc30bab007251947
Luigi Ocone <ocone.luigi123@gmail.com>
2024-02-22 11:37:36 +0100

    added maven project structure and IDE .gitignore

---
+++ src/main/java/it/unisannio/group8/App.java
@@ -0,0 +1,13 @@
+package it.unisannio.group8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
